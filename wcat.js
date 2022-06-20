3c1397b894e09e77d918b7d422d36a415d8110f3
harik6036 <harik8050@gmail.com>
2022-06-20 17:41:01 +0530

    wcat project

---
+++ wcat.js
@@ -0,0 +1,98 @@
+//to do
+
+//const { fstat } = require("fs");
+
+//const { allowedNodeEnvironmentFlags, listeners } = require("process")
+
+//read multiple files
+// wcat -s filepath > convert big line breaks into singular breaks
+// wcat -n filepath> give numbering to all
+// wcat -b filepath> give numbering to non empty lines
+// wcat filename> append all the content
+// wcat -s filename1>filename2> remove spaces and append file in file2
+// if n and b are given execute first command
+let fs = require("fs");
+// input
+let inputArr = process.argv.slice(2);
+// console.log(inputArr);
+// options
+let optionsArr = [];
+let filesArr = [];
+// identify -> options
+for (let i = 0; i < inputArr.length; i++) {
+    let firstChar = inputArr[i].charAt(0);
+    if (firstChar == "-") {
+        optionsArr.push(inputArr[i]);
+    } else {
+        filesArr.push(inputArr[i]);
+
+    }
+
+
+}
+// options check
+let isBothPresent = optionsArr.includes("-b") && optionsArr.includes("-n");
+if (isBothPresent==true) {
+    console.log("either enter -n or -b option");
+    return;
+}
+// existence
+for (let i = 0; i < filesArr.length; i++) {
+    //    /buffer
+    let isPresent = fs.existsSync(filesArr[i]);
+    if (isPresent == false) {
+        console.log(`file ${filesArr[i]} is not present`);
+        return;
+    }
+}
+// read
+let content = "";
+for (let i = 0; i < filesArr.length; i++) {
+    //    /buffer
+    let bufferContent = fs.readFileSync(filesArr[i]);
+    content += bufferContent + "\r\n";
+}
+// console.log(content)
+let contentArr = content.split("\r\n");
+// console.log(contentArr);
+// -s 
+let isSPreset = optionsArr.includes("-s");
+if (isSPreset == true) {
+    for (let i = 1; i < contentArr.length; i++) {
+        if (contentArr[i] == "" && contentArr[i - 1] == "") {
+            contentArr[i] = null;
+        } else if (contentArr[i] == "" && contentArr[i - 1] == null) {
+            contentArr[i] = null;
+        }
+    }
+    let tempArr = [];
+    for (let i = 0; i < contentArr.length; i++) {
+        if (contentArr[i] != null) {
+            tempArr.push(contentArr[i])
+        }
+    }
+    contentArr = tempArr;
+}
+console.log("`````````````````````")
+// console.log(contentArr.join("\n"));
+let isNPresent = optionsArr.includes("-n");
+if (isNPresent == true) {
+    for (let i = 0; i < contentArr.length; i++) {
+        contentArr[i] = `${i + 1} ${contentArr[i]} `;
+    }
+}
+// console.log(contentArr.join("\n"));
+let isBPresent = optionsArr.includes("-b");
+if (isBPresent == true) {
+
+    let counter = 1
+    for (let i = 0; i < contentArr.length; i++) {
+        if (contentArr[i] != "") {
+            // contentArr[i] = `${i + 1} ${contentArr[i]} `;
+            contentArr[i] = `${counter} ${contentArr[i]}`;
+            counter++;
+        }
+    }
+}
+console.log(contentArr.join("\n"));
+
